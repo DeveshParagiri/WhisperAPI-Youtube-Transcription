1003d7f2d0a14e9e812b70a3421c70902d18ace2
DeveshParagiri <devesh.paragiri@gmail.com>
2023-04-10 14:47:12 +0530

    Initial commit

---
+++ chunking.py
@@ -0,0 +1,42 @@
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+from videoproc import youtube_preprocess
+import math
+import os
+
+def chunk_by_size(my_file):
+    myaudio = AudioSegment.from_file(my_file)
+    channel_count = myaudio.channels    #Get channels
+    sample_width = myaudio.sample_width #Get sample width
+    duration_in_sec = len(myaudio) / 1000 #Length of audio in seconds
+    sample_rate = myaudio.frame_rate
+    bit_depth = sample_width * 8 #Bit depth
+
+    wav_file_size = (sample_rate * bit_depth * channel_count * duration_in_sec) / 8
+
+    file_split_size = 22000000  # 24Mb OR 24,000,000 bytes
+
+    total_chunks = int(wav_file_size//file_split_size) + 1
+
+    '''
+    Get chunk size by the following method: 
+    For X duration_in_sec (X) -->  Y wav_file_size
+    For K duration in sec  (K) --> For 24MB file size
+    K = X * 10Mb / Y
+    '''
+
+    chunk_length_in_sec = math.ceil((duration_in_sec * file_split_size)/wav_file_size)   #in seconds
+    chunk_length_ms = chunk_length_in_sec * 1000
+    chunks = make_chunks(myaudio, chunk_length_ms)
+
+    os.mkdir("process_chunks")
+
+    #Export all of the individual chunks as wav files
+    for i, chunk in enumerate(chunks):
+        chunk_name = f"process_chunks/chunk{i}.wav"
+        chunk.export(chunk_name, format="wav")
+
+    return total_chunks
+
+# audio_file = youtube_preprocess('https://www.youtube.com/watch?v=UU-iWa0Hroc')
+# chunk_by_size(audio_file)
---
+++ videoproc.py
@@ -0,0 +1,42 @@
+from moviepy.editor import *
+import pytube
+import os
+import sys
+
+
+
+def youtube_preprocess(link, start = None, end = None):
+    yt = pytube.YouTube(link)
+    yt.register_on_progress_callback(show_progress_bar)
+    destination = '.'
+    video = yt.streams.filter(only_audio = True).first()
+
+    out_file = video.download(output_path=destination)
+
+    base, ext = os.path.splitext(out_file)
+
+    new_file = base + '.wav'
+    os.rename(out_file, new_file)
+
+    if start and end:
+        print(start,end)
+        myclip = AudioFileClip(new_file).subclip(get_sec(start),get_sec(end))
+        myclip.write_audiofile(new_file)
+    return new_file
+
+
+# Display a download progress bar
+def show_progress_bar(stream, _chunk, bytes_remaining):
+  current = ((stream.filesize - bytes_remaining)/stream.filesize)
+  percent = ('{0:.1f}').format(current*100)
+  progress = int(50*current)
+  status = '█' * progress + '-' * (50 - progress)
+  sys.stdout.write(' ↳ |{bar}| {percent}%\r'.format(bar=status, percent=percent))
+  sys.stdout.flush()
+
+def get_sec(time_str):
+    """Get seconds from time.
+    """
+
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
